9c2d6956f1548e3bb806b8824bba6748ec647fd9
Kiril Vasilev <kvasilev@perimetermed.com>
2020-07-05 01:00:45 -0400

    Added new class GameFrame.java.

---
+++ Pacman/src/pacmanGame/GameFrame.java
@@ -0,0 +1,29 @@
+package pacmanGame;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+	public GameFrame() {
+		
+		initUI();
+	}
+	
+	private void initUI() {
+
+        setTitle("Pacman");
+        setSize(640, 480);
+        
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+	
+	public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+        	GameFrame gameFrame = new GameFrame();
+            gameFrame.setVisible(true);
+        });
+    }
+}
