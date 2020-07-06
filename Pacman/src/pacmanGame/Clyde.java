02ce8c227380a68e9119f9b1db89290accf4e91e
borisvassilev <borisvassilev@localhost>
2020-07-06 18:21:03 -0400

    inserted image in clyde and pinky classes and created class for TargettingSystem.

--- Pacman/src/pacmanGame/Clyde.java
+++ Pacman/src/pacmanGame/Clyde.java
@@ -1,5 +1,16 @@
 package pacmanGame;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+
 public class Clyde extends Ghost {
 
+	private Image yellowGhost;
+	
+	public Clyde() {
+		ImageIcon clydeImage = new ImageIcon("Pacman/src/resources/clyde_1.png");
+		yellowGhost = clydeImage.getImage();
+	}
 }
--- Pacman/src/pacmanGame/Pinky.java
+++ Pacman/src/pacmanGame/Pinky.java
@@ -1,5 +1,15 @@
 package pacmanGame;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
 public class Pinky extends Ghost {
 
+	private Image pinkGhost;
+	
+	public Pinky() {
+		ImageIcon pinkyImage = new ImageIcon("Pacman/src/resources/pinky_1.png");
+		pinkGhost = pinkyImage.getImage();
+	}
 }
