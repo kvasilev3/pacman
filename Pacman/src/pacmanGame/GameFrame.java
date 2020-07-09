023dc7c8a3e3b00f748ede9b60699715722ace79
Borisonekenobi <borisonekenobi@gmail.com>
2020-07-09 17:00:48 -0400

    Changed maze to exactly match maze size

--- Pacman/src/pacmanGame/GameFrame.java
+++ Pacman/src/pacmanGame/GameFrame.java
@@ -16,14 +16,14 @@ public class GameFrame extends JFrame {
 		add(Board.getSingleton());
 		
         setTitle("Pacman original 2.0");
-        setSize(646, 509);
+        setSize(392, 457);
         
         ImageIcon myAppImage = new ImageIcon("Pacman/src/resources/pacman_desktop.png");
         setIconImage(myAppImage.getImage());
         
         setLocationRelativeTo(null);
         setResizable(false); //Temporary (Will be switched to true when main game is done)
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        
     }
 	
