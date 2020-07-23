03d81ce4b99ac0350e7586c128a0ffc6b228b796
4boris <4boris6@gmail.com>
2020-07-23 14:49:56 -0400

    Added "PACMAN" label top left and score in top right.
    Set debugger to false to see how game would look.

--- Pacman/src/pacmanGame/Board.java
+++ Pacman/src/pacmanGame/Board.java
@@ -69,7 +69,7 @@ public class Board extends JPanel {
 	
 	private ImageIcon pacmanLives = new ImageIcon("Pacman/src/resources/pacman_right_1.png");
 	
-	private boolean debuggerMode = true;
+	private boolean debuggerMode = false;
 	private ImageIcon grid = new ImageIcon("Pacman/src/resources/tiles_grid.png");
 	private ImageIcon blinkyTarget = new ImageIcon("Pacman/src/resources/blinky_target.png");
 	private ImageIcon inkyTarget = new ImageIcon("Pacman/src/resources/inky_target.png");
@@ -305,8 +305,11 @@ public class Board extends JPanel {
 		
 		g2d.setColor(Color.WHITE);
 		g2d.setFont(new Font("Arial", Font.BOLD, 15));
-		g2d.drawString(pacmanScore, getCenteredTextCoordinates(g2d, pacmanScore)[0], getCenteredTextCoordinates(g2d, pacmanScore)[1]);
-		g2d.drawString("SCORE:", getCenteredTextCoordinates(g2d, "SCORE:")[0], 15);
+		g2d.drawString("SCORE:", (int) (getWidth() - 120), 15);
+		g2d.drawString(pacmanScore, (int) (getWidth() - 100), getCenteredTextCoordinates(g2d, pacmanScore)[1]);
+		
+		g2d.setFont(new Font("Arial", Font.BOLD, 25));
+		g2d.drawString("PACMAN", 60, getCenteredTextCoordinates(g2d, pacmanScore)[1]);
 	}
 	
 	private int[] getCenteredTextCoordinates(Graphics2D g2d, String text) {
